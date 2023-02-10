05a0c561cb379f9ff1536787f5bbd7bb605a8560
Victor Hugo B <victorhugobeppler@gmail.com>
2023-02-09 23:20:11 -0300

    Fix(resolve margin-top in HabitForm)

--- src/components/NewHabitForm.tsx
+++ src/components/NewHabitForm.tsx
@@ -6,6 +6,7 @@ export function NewHabitForm() {
             <label htmlFor="title" className="font-semibold leading-tight">
                 Qual seu comprometimento?
             </label>
+
             <input
                 type="text"
                 id="title"
@@ -14,11 +15,11 @@ export function NewHabitForm() {
                 autoFocus
             />
 
-
             <label htmlFor="" className="font-semibold leading-tight mt-4">
                 Qual a frequência?
             </label>
-            <button type="submit" className="mt-06 rounded-lg p-4 flex items-center gap-3 justify-center font-semibold bg-green-600 hover:bg-green-500">
+            
+            <button type="submit" className="mt-6 rounded-lg p-4 flex items-center gap-3 justify-center font-semibold bg-green-600 hover:bg-green-500">
                 <Check size={20} weight="bold" />
                 Confirmar
             </button>
