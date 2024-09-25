0fa7eaf80e30d44b6e2adc12bddc377b266760b6
victorhugb <victorhugob@hypergroup.com.br>
2024-09-25 00:19:18 -0300

    Refactor SummaryTable component for improved readability and maintainability

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -1,78 +1,63 @@
-import dayjs from "dayjs";
 import { useEffect, useState } from "react";
 import { api } from "../lib/axios";
-import { GenerateDatesFromYearBiginning } from "../utils/generate-dates-from-year-biginning";
+import { GenerateDatesFromCurrentMonth } from "../utils/generate-dates-from-year-biginning";
 import { HabitDay } from "./HabitDay";
+import dayjs from "dayjs";
 
-const weekDays = [
-    'D',
-    'S',
-    'T',
-    'Q',
-    'Q',
-    'S',
-    'S',
-];
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 
-const summaryDates = GenerateDatesFromYearBiginning()
-
-const minimumSummaryDatesSize = 18 * 7 // 18 weeks
-const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
+const summaryDates = GenerateDatesFromCurrentMonth();
 
 type Summary = {
-    id: string;
-    date: string;
-    amount: number;
-    completed: number;
-}[]
+  id: string;
+  date: string;
+  amount: number;
+  completed: number;
+}[];
 
 export function SummaryTable() {
+  const [summary, setSummary] = useState<Summary>([]);
 
-    const [summary, setSummary] = useState<Summary>([])
+  useEffect(() => {
+    api.get("/summary").then((response) => {
+      setSummary(response.data);
+    });
+  }, []);
 
-    useEffect(() => {
-        api.get('/summary').then(response => {
-            setSummary(response.data)
-        })
-    }, [])
-    return (
-        <div className="w-full flex">
-            <div className="grid grid-rows-7 grid-flow-row gap-3">
-                {weekDays.map((weekDay, i) => {
-                    return (
-                        <div
-                            key={`${weekDay}-${i}`}
-                            className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
-                        >
-                            {weekDay}
-                        </div>
-                    )
-                })}
+  return (
+    <div className="w-full flex justify-center flex-col">
+      <div className="text-2xl font-bold text-zinc-400 mb-4 flex justify-center">
+        {dayjs().format("DD [de] MMMM YYYY")}
+      </div>
+      <div className="flex items-center justify-center gap-3 mb-4">
+        <div className="grid grid-rows-7 grid-flow-row gap-3">
+          {weekDays.map((weekDay, i) => (
+            <div
+              key={`${weekDay}-${i}`}
+              className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
+            >
+              {weekDay}
             </div>
-
-            <div className="grid grid-rows-7 grid-flow-col gap-3">
-                {summary.length && summaryDates.map((date) => {
-
-                    const dayInSummary = summary.find(day => {
-                        return dayjs(date).isSame(day.date, 'day')
-                    })
-
-                    return (
-                        <HabitDay
-                            key={date.toString()}
-                            date={date}
-                            amount={dayInSummary?.amount}
-                            defaultCompleted={dayInSummary?.completed}
-                        />
-                    )
-                })}
-
-                {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
-                    return (
-                        <div key={i} className="w-10 h-10 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
-                    )
-                })}
-            </div>
+          ))}
         </div>
-    );
+
+        <div className="grid grid-rows-7 grid-flow-col gap-3">
+          {summaryDates.map((date) => {
+            const dayInSummary = summary.find((day) =>
+              dayjs(date).isSame(day.date, "day")
+            );
+
+            return (
+              <HabitDay
+                key={date.toString()}
+                date={date}
+                amount={dayInSummary?.amount}
+                defaultCompleted={dayInSummary?.completed}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
